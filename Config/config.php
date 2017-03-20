2c3feefdf1b7260b522cef9b4e6c9138451ec324
Sinicha Sinicha <Sinicha Sinicha>
2017-03-21 00:14:20 +0100

    Basics of routing

---
+++ Config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+/* Constante de l'application */
+define("DS", DIRECTORY_SEPARATOR);
+define("BASE_PATH_PATTERN", "\/setupmywebsite\/");
+define("DEBUG_MODE", true);
+
+/* Liens des dossiers */
+define("CORE_PATH", "Core".DS);
+define("APP_PATH", "App");
+define("CONTROLLERS_PATH", APP_PATH.DS."Controllers".DS);
+define("MODELS_PATH", APP_PATH.DS."Models".DS);
+define("VIEWS_PATH", APP_PATH.DS."Views".DS);
+define("LOG_PATH", "Logs".DS);
+
+/* Base de données */
+define("DB_USER", "root");
+define("DB_PWD", "password");
+define("DB_HOST", "localhost");
+define("DB_NAME", "setupmyweb");
+define("DB_PORT", "3306");
