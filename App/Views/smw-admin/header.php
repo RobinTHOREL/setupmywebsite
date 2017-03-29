4e5e4a8f343df7b57e99c2b60a69ed567d9da5b9
Sinicha <sinirados@gmail.com>
2017-03-29 20:22:27 +0200

    merge dashboard on routing

--- App/Views/smw-admin/header.php
+++ App/Views/smw-admin/header.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="App/Views/smw-admin/css/style.css">
-    <link rel="stylesheet" href="App/Views/smw-admin/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="Public/css/style.css">
+    <link rel="stylesheet" href="Public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 </head>
 <body>
--- App/Views/smw-admin/menu_gauche.tpl.php
+++ App/Views/smw-admin/menu_gauche.tpl.php
@@ -5,69 +5,69 @@
     <div class="accordion">
         <div class="section">
             <input type="radio" name="accordion-1" id="section-1" checked="checked"/>
-            <label for="section-1"><i class="fa fa-eye"></i><span> Dashboard</span></label>
+            <label for="section-1"><i class="fa fa-eye respons_hidden"></i><span class="respons_hidden">Dashboard</span></label>
         </div>
         <div class="section">
             <input type="radio" name="accordion-1" id="section-2" value="toggle"/>
-            <label for="section-2"><i class="fa fa-pencil"></i> <span>Articles</span></label>
+            <label for="section-2"><i class="fa fa-pencil"></i> <span class="respons_hidden">Articles</span></label>
             <div class="content">
                 <ul>
-                    <li><i class="fa fa-plus"></i><span>Ajouter</span></li>
-                    <li><i class="fa fa-bars"></i><span>Liste des articles</span></li>
+                    <li><i class="fa fa-plus"></i><span class="respons_hidden">Ajouter</span></li>
+                    <li><i class="fa fa-bars"></i><span class="respons_hidden">Liste des articles</span></li>
                 </ul>
             </div>
         </div>
         <div class="section">
             <input type="radio" name="accordion-1" id="section-3" value="toggle"/>
-            <label for="section-3"><i class="fa fa-file-o"></i> <span>Pages</span></label>
+            <label for="section-3"><i class="fa fa-file-o"></i> <span class="respons_hidden">Pages</span></label>
             <div class="content">
                 <ul>
-                    <li><i class="fa fa-plus"></i><span>Ajouter</span></li>
-                    <li><i class="fa fa-bars"></i><span>Toutes les pages</span></li>
+                    <li><i class="fa fa-plus"></i><span class="respons_hidden">Ajouter</span></li>
+                    <li><i class="fa fa-bars"></i><span class="respons_hidden">Toutes les pages</span></li>
                 </ul>
             </div>
         </div>
         <div class="section">
-            <input type="radio" name="accordion-1" id="section-4" value="toggle"/>
+            <input type="radio" name="accordion-1" id="section-4" value="toggle" />
             <label for="section-4"><i class="fa fa-camera"></i> <span>Bibliothéque</span></label>
             <div class="content">
                 <ul>
-                    <li><i class="fa fa-plus"></i><span>Ajouter un média</span></li>
-                    <li><i class="fa fa-th"></i><span>Bibliothéque des médias</span></li>
+                    <li><i class="fa fa-plus"></i><span class="respons_hidden">Ajouter un média</span></li>
+                    <li><i class="fa fa-th"></i><span class="respons_hidden">Bibliothéque des médias</span></li>
                 </ul>
             </div>
         </div>
         <div class="section">
             <input type="radio" name="accordion-1" id="section-5" value="toggle"/>
-            <label for="section-5"><i class="fa fa-font"></i> <span>Apparence</span></label>
+            <label for="section-5"><i class="fa fa-font"></i> <span class="respons_hidden">Apparence</span></label>
             <div class="content">
                 <ul>
-                    <li><i class="fa fa-desktop"></i><span>Thémes</span></li>
-                    <li><i class="fa fa-eyedropper"></i><span>Personnaliser</span></li>
-                    <li><i class="fa fa-level-up"></i><span>Widget</span></li>
+                    <li><i class="fa fa-desktop"></i><span class="respons_hidden">Thémes</span></li>
+                    <li><i class="fa fa-eyedropper"></i><span class="respons_hidden">Personnaliser</span></li>
+                    <li><i class="fa fa-level-up"></i><span class="respons_hidden">Widget</span></li>
                 </ul>
             </div>
         </div>
         <div class="section">
             <input type="radio" name="accordion-1" id="section-6" value="toggle"/>
-            <label for="section-6"><i class="fa fa-user-circle"></i> <span>Utilisateurs</span></label>
+            <label for="section-6"><i class="fa fa-user-circle"></i> <span class="respons_hidden">Utilisateurs</span></label>
             <div class="content">
                 <ul>
-                    <li><i class="fa fa-user-plus"></i><span>Ajouter</span></li>
-                    <li><i class="fa fa-users"></i><span>Tous les utilisateurs</span></li>
-                    <li><i class="fa fa-user-o"></i><span>Votre profil</span></li>
+                    <li><i class="fa fa-user-plus"></i><span class="respons_hidden">Ajouter</span></li>
+                    <li><i class="fa fa-users"></i><span class="respons_hidden">Tous les utilisateurs</span></li>
+                    <li><i class="fa fa-user-o"></i><span class="respons_hidden">Votre profil</span></li>
                 </ul>
             </div>
         </div>
         <div class="section">
             <input type="radio" name="accordion-1" id="section-7" value="toggle"/>
-            <label for="section-7"><i class="fa fa-wrench"></i> <span>Outils</span></label>
+            <label for="section-7"><i class="fa fa-wrench"></i> <span class="respons_hidden">Outils</span></label>
             <!--<div class="content">
             </div>-->
         </div>
         <div class="section">
             <input type="radio" name="accordion-1" id="section-8" value="toggle"/>
-            <label for="section-8"><i class="fa fa-gears"></i> <span>Réglages</span></label>
+            <label for="section-8"><i class="fa fa-gears"></i> <span class="respons_hidden">Réglages</span></label>
             <!--  <div class="content">
               </div> -->
         </div>
