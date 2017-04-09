ed62aabab6c0c84c3edd09b68c23bef115e6a5f3
Sinicha Sinicha <Sinicha Sinicha>
2017-04-09 23:50:40 +0200

    Add populate on BaseSql class

--- Core/BaseSql.class.php
+++ Core/BaseSql.class.php
@@ -21,47 +21,78 @@
 			$this->columns = array_diff_key($userVars, $sqlVars);
 		}
 
-		public function Save() {
-			// Si id = -1 faire un insert dynamique
-			if( $this->id == -1) {
-				unset($this->columns["id"]);
-				$sqlCol = null;
-				$sqlKey = null;
-				foreach($this->columns as $column => $value) {
-					$data[$column] = $this->$column;
-					$sqlCol .= ",".$column;
-					$sqlKey .= ",:".$column;
-				}
-				$sqlCol=ltrim($sqlCol, ",");
-				$sqlKey=ltrim($sqlKey, ",");
+		public function Save()
+        {
+            // Si id = -1 faire un insert dynamique
+            if ($this->id == -1) {
+                unset($this->columns["id"]);
+                $sqlCol = null;
+                $sqlKey = null;
+                foreach ($this->columns as $column => $value) {
+                    $data[$column] = $this->$column;
+                    $sqlCol .= "," . $column;
+                    $sqlKey .= ",:" . $column;
+                }
+                $sqlCol = ltrim($sqlCol, ",");
+                $sqlKey = ltrim($sqlKey, ",");
 
-				$query = $this->db->prepare(
-							"INSERT INTO ".$this->table ." (".$sqlCol.")
-							VALUES (".$sqlKey.") ;"
-							);
+                $query = $this->db->prepare(
+                    "INSERT INTO " . $this->table . " (" . $sqlCol . ")
+							VALUES (" . $sqlKey . ") ;"
+                );
+                $query->execute($data);
+            } else {
+                // Sinon faire un update dynamique
+                $sqlSet = null;
+                foreach ($this->columns as $column => $value) {
+                    $data[$column] = $this->$column;
+                    $sqlSet[] .= $column . "=:" . $column;
+                }
 
-				print_r($data);
-				$query->execute($data);
-			} else {
-			// Sinon faire un update dynamique
-				$sqlSet = null;
-				foreach($this->columns as $column => $value) {
-					$data[$column] = $this->$column;
-					$sqlSet[] .= $column ."=:".$column;
-				}
-
-				$query = $this->db->prepare(
-							"UPDATE ".$this->table ."
+                $query = $this->db->prepare(
+                    "UPDATE " . $this->table . "
 							SET date_updated = sysdate(), 
-							".implode(",", $sqlSet)."
+							" . implode(",", $sqlSet) . "
 							WHERE id=:id;"
-						);
+                );
 
-				echo "UPDATE ".$this->table ."
+                echo "UPDATE " . $this->table . "
 							SET date_updated = sysdate(), 
-							".implode(",", $sqlSet)."
+							" . implode(",", $sqlSet) . "
 							WHERE id=:id;";
-				$query->execute($data);
-			}
-		}
+                $query->execute($data);
+            }
+        }
+
+        // La fonction a pour but d'alimenter l'objet suite à une requête SQL (Attention la requete ne doit retourner
+        //  qu'une seule valeur.
+        public function populate( $condition = ["id"=>1]) {
+            // Requete SQL
+            $req = "SELECT * FROM " .$this->table. " WHERE ";
+            $i = 1;
+            $count = count($condition);
+            foreach( $condition as $key => $val) {
+                $req .= $key. " = " .$val. "";
+                if ($count > 1 && $i < $count) {
+                    $req .= " AND ";
+                }
+                $i = $i + 1;
+            }
+
+            $query = $this->db->prepare($req );
+            $query->execute();
+            $results = $query->fetch(PDO::FETCH_ASSOC);
+
+            // Vérification
+            if (count($results) <= 1) {
+                return;
+            }
+
+            // Alimentation de l'objet
+            foreach($results as $key => $val) {
+                $this->{$key} = $val;
+            }
+
+        }
+
 	}
