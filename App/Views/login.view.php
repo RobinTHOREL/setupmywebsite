b7a4f4dcfc57441e69308db4c31db682d215ce33
nanaonesie <nanaonesie73@gmail.com>
2017-04-16 02:03:57 +0200

    Ajout de la page login avec quelques modification avenir

--- App/Views/login.view.php
+++ App/Views/login.view.php
@@ -1,15 +1,38 @@
-<h1>Login</h1>
-<form action="" method="post">
-    <?php
-        if( !empty($error) ) {
-            echo "<p>" .htmlspecialchars($error). "</p>";
-        }
-    ?>
+<D<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/setupmywebsite/Public/css/grid.css">
+    <link rel="stylesheet" href="/setupmywebsite/Public/css/styleLogin.css">
+    <meta charset="UTF-8">
+    <title>Page de connexion</title>
+</head>
+    <body>
+        <div class="back_glob">
+            <div class="back_header">
+                <h1>LOGIN</h1>
+            </div>
+            <form action="" method="post">
+                <div class="trois-colonnes">
+                    <div class="colonne2">
+                        <input type="text" name="login" value="admin" class="form-group">
+                    </div>
 
-        <label for="login">Identifiant : </label>
-        <input type="text" name="login" id="login" value="admin"><br>
-        <label for="password">Mot de passe : </label>
-        <input type="password" name="password" id="password" value="admin">
-        <input type="submit" name="submit" value="Login">
-    </div>
-</form>
+                    <div class="colonne2">
+                         <input type="password" placeholder="password" name="password" value="admin" class="form-group">
+                    </div>
+
+                    <div class="colonne2">
+                        <input type="submit" name="login" value="Login" class="form-group">
+                    </div>
+                </div>
+                    <a href="#">Forgot password?</a>
+                <?php
+                if( !empty($error) ) {
+                    echo "<p>" .htmlspecialchars($error). "</p>";
+                }
+                ?>
+            </form>
+        </div>
+    </body>
+</html>
+
