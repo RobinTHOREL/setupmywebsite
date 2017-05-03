3f5da1f4bc086b72988027fffa9f9f8fb6a91562
younesdiouri <younesdiouri@gmail.com>
2017-05-03 18:41:57 +0200

    ajout du csrf

--- Core/Helpers.class.php
+++ Core/Helpers.class.php
@@ -1,4 +1,31 @@
 <?php
-	class Helpers {
-		
-	}
+    class Helpers {
+
+
+        //Cette fonction génère, sauvegarde et retourne un token
+        //Vous pouvez lui passer en paramètre optionnel un nom pour différencier les formulaires
+        function generer_token($nom = '')
+        {
+            session_start();
+            $token = uniqid(rand(), true);
+            $_SESSION[$nom.'_token'] = $token;
+            $_SESSION[$nom.'_token_time'] = time();
+            return $token;
+        }
+
+
+        //Cette fonction vérifie le token
+        //Vous passez en argument le temps de validité (en secondes)
+        //Le referer attendu (adresse absolue, rappelez-vous :D)
+        //Le nom optionnel si vous en avez défini un lors de la création du token
+        function verifier_token($temps, $referer, $nom = '')
+        {
+            session_start();
+            if(isset($_SESSION[$nom.'_token']) && isset($_SESSION[$nom.'_token_time']) && isset($_POST['token']))
+                if($_SESSION[$nom.'_token'] == $_POST['token'])
+                    if($_SESSION[$nom.'_token_time'] >= (time() - $temps))
+                        if($_SERVER['HTTP_REFERER'] == $referer)
+                            return true;
+            return false;
+        }
+    }
