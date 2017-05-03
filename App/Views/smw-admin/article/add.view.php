94c7630a94621f85444a62d55f1588d384043f5a
brixton <thorelrobin@yahoo.fr>
2017-05-03 19:25:07 +0200

    Add form-group'

--- App/Views/smw-admin/article/add.view.php
+++ App/Views/smw-admin/article/add.view.php
@@ -5,13 +5,34 @@ include(dirname(__DIR__).'/menu_gauche.tpl.php');
 
 <div class="container">
     <div class="row"> <!-- exemple - ligne 1 -->
-        <div class="col-10 col-offset-1 bordureRouge" style="border: 2px solid red; padding: 10px;"></div>
+        <div class="col-10 title">
+            Ajouter un article
+        </div>
     </div>
     <div class="row"> <!-- exemple - ligne 2 -->
-        <div class="col-6 col-offset-3 bordureVerte" style="border: 2px solid green; padding: 10px;"></div>
+        <div class="col-7">
+            <input type="text" class="form-group" placeholder="Titre de l'article">
+        </div> <!-- exemple - ligne 2 -->
+        <div class="col-4 col-offset-1">
+            <textarea type="text" class="form-group" placeholder="Titre de l'article"> </textarea>
+        </div>
     </div>
     <div class="row"> <!-- exercice - ligne 3 -->
-        <div class="col-2 col-offset-2 bordureBleue" style="border: 2px solid blue; padding: 10px;"></div>
+        <div class="col-3">
+            <select name="salut" id="salut" class="form-group">
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+            </select>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1 col-offset-1">
+            <input type="submit">
+        </div>
+        <div class="col-1 col-offset-1">
+            <input type="reset">
+        </div>
     </div>
 </div>
 
