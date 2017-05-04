79278fa8a0ed98dc83ab3dfbd97eb1362b6be109
Sinicha Sinicha <Sinicha Sinicha>
2017-05-05 00:24:17 +0200

    Connect to MySql and execute SQL file

--- Install/Validation-configuration.php
+++ Install/Validation-configuration.php
@@ -1,9 +1,28 @@
 ﻿<?php
 if( !empty($_POST['host']) && !empty($_POST['port']) && !empty($_POST['user']) 
 	&& isset($_POST['password']) && !empty($_POST['database_name']) ) {
+	// On récupère les données dans des variables et on les vérifie
 	$host = $_POST['host'];
 	$port = $_POST['port'];
 	$user = $_POST['user'];
 	$password = $_POST['password'];
 	$databaseName = $_POST['database_name'];
+
+	// On essaye de se connecter à la base de données avec les données reçu
+	try {
+        $pdo = new PDO("mysql:host=".$host.";dbname=".$databaseName.";port=".$port, $user, $password);
+	} catch(Exception $e) {
+		echo "Erreur de connexion.";
+		die();
+	}
+
+	// Si la connexion à fonctionné, on charge le fichier SQL et on execute le contenu
+	$sqlFile = "../setupmyweb.sql";
+    $req="";
+    $req=file_get_contents ($sqlFile);
+    $pdo->query($req);
+
+	// On vérifie si les données ont bien été créé
+    $is_table=$pdo->query("SHOW TABLES LIKE 'users'");
+    echo "Row:" .$is_table->rowCount();
 }
