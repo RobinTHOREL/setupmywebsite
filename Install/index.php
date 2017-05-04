08d784cdd7301bc102b44e2c75f812f2b613e268
Sinicha Sinicha <Sinicha Sinicha>
2017-05-04 23:55:07 +0200

    create Install folder with index file

---
+++ Install/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"> 
+	<title>Bienvenue sur l'installation de Setup-My Website</title>
+	<meta name="description" content="Page d'installation de Setup-My Website">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+	    Bienvenue sur Setup-My Website<br>
+	    <a href="">Commencer l'installation<a/>
+    </header>
+</body>
+</html>   
