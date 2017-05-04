54fb034cf5dc1c5b44bd0c640665d37fdeea8e88
Sinicha Sinicha <Sinicha Sinicha>
2017-05-05 00:12:56 +0200

    add Validation-configuration file that validate input configurations

---
+++ Install/Validation-configuration.php
@@ -0,0 +1,9 @@
+﻿<?php
+if( !empty($_POST['host']) && !empty($_POST['port']) && !empty($_POST['user']) 
+	&& isset($_POST['password']) && !empty($_POST['database_name']) ) {
+	$host = $_POST['host'];
+	$port = $_POST['port'];
+	$user = $_POST['user'];
+	$password = $_POST['password'];
+	$databaseName = $_POST['database_name'];
+}
