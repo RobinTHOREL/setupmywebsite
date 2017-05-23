e8463c28670445ffde87084b5f36644765af1eaa
brixton <thorelrobin@yahoo.fr>
2017-05-23 10:03:36 +0200

    Edit form-group

--- App/Views/smw-admin/article/add.view.php
+++ App/Views/smw-admin/article/add.view.php
@@ -34,6 +34,36 @@ include(dirname(__DIR__).'/menu_gauche.tpl.php');
             <input type="reset">
         </div>
     </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="col-6">
+                <!--Code for Radio Buttons-->
+                <input type="radio" id="rb1" name="rb" value="" checked class="form-group">
+                <label for="rb1">Article</label><br>
+                <input type="radio" id="rb2" name="rb"value="" class="form-group">
+                <label for="rb2">Page</label><br>
+                <input type="radio" id="rb3" name="rb" value="" class="form-group">
+                <!--  <label for="rb3">United States</label> -->
+            </div>
+            <div class="col-6">
+                <!--Code for Checkbox-->
+                <input type="checkbox" id="cb1" name="cb" value="" class="form-group">
+                <label for="cb1">Activer les commentaires</label><br>
+                <input type="checkbox" id="cb2" name="cb"value="" checked class="form-group">
+                <label for="cb2">-----???</label><br>
+                <input type="checkbox" id="cb3" name="cb" value="" class="form-group">
+                <label for="cb3">Indiqué la date et l'heure de la publication</label>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <input type="email" class="form-group" placeholder="example@example.com">
+        </div>
+        <div class="col-2 col-offset-1">
+            <input type="number" class="form-group" value="1" min="0">
+        </div>
+    </div>
 </div>
 
 
