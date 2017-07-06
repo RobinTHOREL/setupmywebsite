35f27e8d02f70ec513b45c84faa79a869a7d7e0f
Sinicha <sinirados@gmail.com>
2017-07-06 13:07:24 +0200

    add templates folder with default template apply on index

---
+++ App/Views/templates/Default/frontend.temp.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title><?php echo $page_title ?></title>
+		<meta name="description" content="<?php echo $page_description ?>">
+	</head>
+	<body>
+			<?php include $this->view; ?>
+	</body>
+</html>
