723bbb47eb9a9c64385e3f201157bc0aec7681f3
nanaonesie <nanaonesie73@gmail.com>
2017-07-13 14:10:54 +0200

    update front chart et front media

--- App/Views/smw-admin/medias/add.view.php
+++ App/Views/smw-admin/medias/add.view.php
@@ -1,21 +1,24 @@
-    <div class="container">
-        <div class="row"> <!-- exemple - ligne 1 -->
-            <div class="col-10 title">
-                <h2>Envoyer un média</h2>
-            </div>
-            <div class="row"> <!-- exemple - ligne 2 -->
-                <div class="col-7">
-                    <input type="text" class="form-group col-4 col-offset-1" placeholder="Nom du média">
-                </div> <!-- exemple - ligne 2 -->
-                <div class="col-10 col-offset-1">
-                    <div id="dropfile">
-                        <div id="dropper" class="col-8 col-offset-2">
-                            <i class="fa fa-cloud-upload logo-upload" aria-hidden="true"></i>
-                            <p>Glissez un média dans cette zone pour l'uploader</p>
-                            <p>Formats acceptés : (png, ..,..,..)</p>
-                        </div>
+<div class="container">
+    <div class="row"> <!-- exemple - ligne 1 -->
+        <div class="col-10 title">
+            <h2>Envoyer un média</h2>
+        </div>
+        <div class="row"> <!-- exemple - ligne 2 -->
+            <div class="col-7">
+                <input type="text" class="form-group col-4 col-offset-1" placeholder="Nom du média">
+            </div> <!-- exemple - ligne 2 -->
+            <div class="col-10 col-offset-1">
+                <div id="dropfile">
+                    <div id="dropper" class="col-8 col-offset-2" class="col-1">
+                        <i class="fa fa-cloud-upload logo-upload" aria-hidden="false"></i>
+                        <p>Glissez un média dans cette zone pour l'uploader</p>
+                        <p>Formats acceptés : (png, ..,..,..)</p>
                     </div>
+                </div>
+                <div id='hide'>
+                    <input type='button' name='upload' value='télécharger un fichier' />
                 </div>
             </div>
         </div>
     </div>
+</div>
