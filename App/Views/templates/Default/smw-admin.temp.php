a45d426e24db4501abdded22a262f7473290719d
Sinicha <sinirados@gmail.com>
2017-07-18 22:53:31 +0200

    Create MODE_PRODUCTION constant and add offline tinyMCE

--- App/Views/templates/Default/smw-admin.temp.php
+++ App/Views/templates/Default/smw-admin.temp.php
@@ -5,17 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $page_title;
-        ?></title>
+    <title><?php echo $page_title; ?></title>
     <meta name="description" content="<?php echo $page_description ?>">
     <!--   Need to change the document root ($server) to a public folder -->
     <link rel="stylesheet" href="/setupmywebsite/Public/css/style.css">
     <link rel="stylesheet" href="/setupmywebsite/Public/css/grid.css">
     <link rel="stylesheet" href="/setupmywebsite/Public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="icon" href="/setupmywebsite/Public/img/favicon.ico" />
-    <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+    <?php // Chargement de TinyMCE avec CDN en mode Production
+        if(PRODUCTION_MODE === true) {
+            echo '<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>';
+        } else {
+            echo '<script src="'.ABSOLUTE_PATH_FRONT.PUBLIC_PATH.'/js/tinymce/tinymce.min.js"></script>';
+        }
+    ?>
+    
     <script>tinymce.init({ selector:'textarea' });</script>
-
 </head>
 <body>
     <nav>
