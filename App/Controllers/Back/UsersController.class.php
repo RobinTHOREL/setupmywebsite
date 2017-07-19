051b222b2b2949650e35598f7644e96491032790
Sinicha <sinirados@gmail.com>
2017-07-19 13:43:06 +0200

    Update Add action on Users controller

--- App/Controllers/Back/UsersController.class.php
+++ App/Controllers/Back/UsersController.class.php
@@ -2,57 +2,86 @@
 class UsersController{
     public function addAction($params)
     {
-        if($_POST)
+        $view = new View(BASE_BACK_OFFICE."users/add", "smw-admin");
+        if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['login']) && !empty($_POST['password']) && !empty($_POST['passwordConfirm']) && !empty($_POST['email']) && !empty($_POST['firstname'])
+            && !empty($_POST['lastname']) && !empty($_POST['permission']))
         {
 
-            $referer = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]."";
-            if(Helpers::verifier_token(
-                600,
-                $referer ,
-                "useradd")
-            )
+            /* 
+                // Vérification du token
+                $referer = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]."";
+                if(Helpers::verifier_token(
+                    600,
+                    $referer ,
+                    "useradd")
+                )
+                {
+                } 
+            */
+            
+            // On initialise les variables si les données sont bien réupérés
+            $user = new Users();
+            $messages = array();
+            $login = trim($_POST['login']);
+            $password = trim($_POST['password']);
+            $passwordConfirm = trim($_POST['passwordConfirm']);
+            $email = trim($_POST["email"]);
+            $firstname = trim($_POST["firstname"]);
+            $lastname = trim($_POST["lastname"]);
+            $permission = trim($_POST["permission"]);
+            
+            if(strlen($login)<4 || strlen($login)>64) {
+                array_push($messages, "Le login saisie est incorrect. Il doit faire en 4 et 64 caractères.");
+            }
+            
+            $validePassword = new Password(true, true, true, true, 4, 64);
+            $validePassword->setPassword($password);
+            if(!$validePassword->validePassword($password)) {
+                array_push($messages, "Le mot de passe saisie ne respecte pas les critères de sécurités.");
+            }
+            
+            if(strcmp($password, $passwordConfirm) != 0) {
+                array_push($messages, "Les mots de passe saisies ne correspondent pas.");
+            }
+
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                array_push($messages, "Le format de l'email saisie est invalide.");
+            }
+            
+            if(strlen($firstname)>120) {
+                array_push($messages, "La longueur du prénom saisie est trop grande.");
+            }
+            
+            if(strlen($lastname)>120) {
+                array_push($messages, "La longueur du nom saisie est trop grande.");
+            }
+            
+            if(strlen($email)>320) {
+                array_push($messages, "La longueur de l'email saisie est trop grande.");
+            }
+            
+            $checkExist = $user->getAllBy($search = ["OR" => ["login"=>$login, "email"=>$email]]);
+            if($checkExist == true) {
+                array_push($messages, "L'utilisateur existe déjà.");
+            }
+            
+            if( count($messages)<=0 )
             {
-
-                if( !empty($_POST['login']) &&
-                    !empty($_POST['password']) &&
-                    !empty($_POST['passwordConfirm']) &&
-                    !empty($_POST['email']) &&
-                    !empty($_POST['firstname']) &&
-                    !empty($_POST['lastname']) &&
-                    !empty($_POST['permission'])
-                ){
-                    // On initialise les variables si les données sont bien réupérés
-                    $login = trim($_POST['login']);
-                    $password = trim($_POST['password']);
-                    $passwordConfirm = trim($_POST['passwordConfirm']);
-                    $email = trim($_POST["email"]);
-                    $firstname = trim($_POST["firstname"]);
-                    $lastname = trim($_POST["lastname"]);
-                    $permission = trim($_POST["permission"]);
-
-                    $user = new Users();
-                    if( $user->getAllBy($search = [
-                        "OR" => ["login"=>$login, "email"=>$email]]) == false)
-                    {
-                        //On peut enregistrer l'utilisateur, mais son compte ne sera actif que quand il sera enregistré par mail
-                        $user->setLogin($login);
-                        $user->setPassword($password);
-                        $user->setEmail($email);
-                        $user->setFirstName($firstname);
-                        $user->setLastName($lastname);
-                        $user->setStatus(0);
-                        $user->setPermission($permission);
-                        $user->Save();
-                    }
-                    else{
-                        //list of errors a developper ! ! !
-                        echo "on ne peut pas enregistrer l'utilisateur. ";
-                    }
-
-                }
-            }// close verifier_token
-        }//close $_POST
-        $view = new View(BASE_BACK_OFFICE."users/add", "smw-admin");
+                //On peut enregistrer l'utilisateur, mais son compte ne sera actif que quand il sera enregistré par mail
+                $user->setLogin($login);
+                $user->setPassword($password);
+                $user->setEmail($email);
+                $user->setFirstName($firstname);
+                $user->setLastName($lastname);
+                //$user->setStatus(0);
+                //$user->setPermission($permission);
+                $user->setActivationKey("");
+                $user->Save();
+                array_push($messages, "L'utilisateur a été créé.");
+            }
+            
+            $view->assign("messages", $messages);
+        }
         $view->assign("page_title", "Ajouter des utilisateurs");
         $view->assign("page_description", "Page d'ajout d'un utilisateur");
     }
--- App/Views/smw-admin/users/add.view.php
+++ App/Views/smw-admin/users/add.view.php
@@ -5,21 +5,32 @@
             </div>
             <div class="row"> <!-- exemple - ligne 2 -->
                 <div class="col-6 col-offset-3">
+                <?php 
+                    // Liste les messages d'erreurs ou de réussite
+                    echo '<ul>';
+                    if(isset($messages) && is_array($messages) ) { 
+                        foreach($messages as $message)
+                            echo '<li>'.$message.'</li>';
+                        } 
+                    echo '</ul>';
+                ?>
                     <form action="" method="POST">
-                        <?php $token = Helpers::generer_token('useradd');?>
-                        <input type="hidden" name="token" value="<?php echo $token; ?>">
-                        <input type="text" class="form-group" name="login" placeholder="Login">
-                        <input type="text" class="form-group" name="lastname" placeholder="Nom">
-                        <input type="text" class="form-group" name="firstname" placeholder="Prénom">
-                        <input type="email" class="form-group" name="email" placeholder="exemple@exemple.com">
+                        <label>Login<input type="text" class="form-group" name="login" placeholder="Login" required="required"></label>
+                        <label>Mot de passe<input type="password" class="form-group" name="password" placeholder="Mot de passe" required="required"></label>
+                        <label>Confirmation du mot de passe<input type="password" class="form-group" name="passwordConfirm" placeholder="Confirmation du mot de passe" required="required"></label>
+                        <label>Nom<input type="text" class="form-group" name="lastname" placeholder="Nom"></label>
+                        <label>Prénom<input type="text" class="form-group" name="firstname" placeholder="Prénom"></label>
+                        <label>Email<input type="email" class="form-group" name="email" placeholder="exemple@exemple.com" required="required"></label>
                         <!-- envoyer un mail a l'utilisateur pour qu'il set son password ??? -->
-                        <select name="" id="role" class="form-group">
+                        <label>Role<select name="permission" id="role" class="form-group" required="required">
                             <option>Membre</option>
-                        </select>
+                        </select></label>
                         <div class="col-8 col-offset-2" style="text-align: center">
                             <input type="checkbox" id="notif" name="cb" value="" class="form-group">
                             <label for="notif">Notifier le nouveau membre par mail !</label><br>
                         </div>
+                        <?php $token = Helpers::generer_token('useradd');?>
+                        <input type="hidden" name="token" value="<?php echo $token; ?>">
                         <input type="submit" class="">
                     </form>
                 </div> <!-- exemple - ligne 2 -->
