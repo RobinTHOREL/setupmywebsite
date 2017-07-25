76fade82c9be0e9a0f14e52017105b9121fc75c2
nanaonesie <nanaonesie73@gmail.com>
2017-07-25 21:00:12 +0200

    btn upload uptodate

--- App/Views/smw-admin/medias/add.view.php
+++ App/Views/smw-admin/medias/add.view.php
@@ -7,9 +7,11 @@
                 <div class="col-10 col-offset-1">
                     <div id="dropfile">
                         <div id="dropper" class="col-8 col-offset-2">
-                            <button id="dropfile_btn_upload" class="fa fa-cloud-upload logo-upload" aria-hidden="true"></button>
+                            <button id="dropfile_btn_upload" class="fa fa-cloud-upload logo-upload" aria-hidden="true">
+                                <p id="msg2">Cliquez ici</p>
+                            </button>
                             <input type="file" id="dropfile_hidden_upload">
-                            <p>Glissez un média dans cette zone pour l'uploader</p>
+                            <p id="msg1">Glissez un média dans cette zone pour l'uploader</p>
                             <p>Formats acceptés : (png, jpg, jpeg)</p>
                         </div>
                     </div>
